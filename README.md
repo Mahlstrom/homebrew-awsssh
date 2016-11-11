fa41eac06118f10bd7bef6f1553c2c51433e16ec
mahlstrom <magnus.ahlstrom@sbsradio.se>
2016-11-11 15:55:52 +0100

    a

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# blutti
+# AWSSSH
+Please add this to your ~/.bash_profile
+```
+. "/usr/local/etc/bash_completion.d/awsssh"
+```
+
+You have jsjolinkey.pem in ~/.ssh folder
--- awsssh.rb
+++ awsssh.rb
@@ -11,19 +11,8 @@ class Awsssh < Formula
   end
 
   def caveats; <<-EOS.undent
-    Please note that upstream has asked us to make explicit managing
-    nvm via Homebrew is unsupported by them and you should check any
-    problems against the standard nvm install method prior to reporting.
-    You should create NVM's working directory if it doesn't exist:
-      mkdir ~/.nvm
-    Add the following to #{shell_profile} or your desired shell
-    configuration file:
-      export NVM_DIR="$HOME/.nvm"
-      . "#{opt_prefix}/nvm.sh"
-    You can set $NVM_DIR to any location, but leaving it unchanged from
-    #{prefix} will destroy any nvm-installed Node installations
-    upon upgrade/reinstall.
-    Type `nvm help` for further information.
+    Please add this to your ~/.bash_profile
+      . "/usr/local/etc/bash_completion.d/awsssh"
   EOS
   end
 end
